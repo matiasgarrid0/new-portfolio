24d724196fb12f80e98b2ab9091de21e7d6d1fe4
Matias Garrido <matias17garrido@gmail.com>
2022-07-04 02:14:39 -0300

    contact form working

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -3,14 +3,64 @@ import "./Contact.css";
 import Aos from "aos";
 import Cont from "../../Images/contact.png";
 import { TextField, Box } from "@mui/material";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
 
 export const Contact = () => {
+  const [forms, setForm] = useState({
+    user_email: "",
+    user_name: "",
+    message: "",
+    subject: "",
+  });
+  const { user_email, user_name, message, subject } = forms;
+
+  console.log(forms.message.length)
+
   useEffect(() => {
     Aos.init({ duration: 1000 });
   }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!user_email || !user_name || !message) {
+      toast.error("Please complete each field");
+    } else {
+      emailjs.sendForm(
+        "service_b7d2niz",
+        "template_wnaxl8n",
+        e.target,
+        "aAGZij1MXePtV_tKe"
+      );
+      try {
+        toast.success("Email sent successfully");
+        setForm({
+          user_email: "",
+          user_name: "",
+          message: "",
+          subject: "",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+  const handleInputChange = (e) => {
+    setForm({ ...forms, [e.target.name]: e.target.value });
+    if (message.length >= 399) {
+      toast.error("Max of 400 caracters");
+    }
+  };
+
   return (
     <div className="div-container-contact">
       <div className="div-container-elements-experience">
+        <ToastContainer
+          position="top-center"
+          theme="colored"
+          autoClose={2000}
+        />
         <p className="p-title-experience" data-aos="fade-right">
           CONTACT ME
         </p>
@@ -25,58 +75,76 @@ export const Contact = () => {
             <img className="img-contact" src={Cont} alt="" />
           </div>
           <div className="div-card-contact">
-            <Box
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                gap: "20px",
-                marginTop: "-10px",
-              }}
-              className="box-contact"
-            >
-              <div className="container-2-inputs">
-                <div style={{ width: "500px" }}>
-                  <TextField
-                    variant="standard"
-                    type="email"
-                    label="Email"
-                    fullWidth
-                    size="small"
-                    required
-                  />
+            <form onSubmit={(e) => handleSubmit(e)} id="contactForm">
+              <Box
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "20px",
+                  marginTop: "-10px",
+                }}
+                className="box-contact"
+              >
+                <div className="container-2-inputs">
+                  <div style={{ width: "500px" }}>
+                    <TextField
+                      variant="standard"
+                      type="email"
+                      label="Email"
+                      fullWidth
+                      size="small"
+                      required
+                      name="user_email"
+                      value={user_email}
+                      onChange={(e) => handleInputChange(e)}
+                      inputProps={{ maxLength: 60 }}
+                    />
+                  </div>
+                  <div style={{ marginLeft: "10px", width: "500px" }}>
+                    <TextField
+                      variant="standard"
+                      type="text"
+                      label="Name"
+                      fullWidth
+                      size="small"
+                      required
+                      name="user_name"
+                    value={user_name}
+                      onChange={(e) => handleInputChange(e)}
+                      inputProps={{ maxLength: 60 }}
+                    />
+                  </div>
                 </div>
-                <div style={{ marginLeft: "10px", width: "500px" }}>
-                  <TextField
-                    variant="standard"
-                    type="text"
-                    label="Name"
-                    fullWidth
-                    size="small"
-                    required
-                  />
+                <TextField
+                  variant="standard"
+                  type="text"
+                  label="Subject"
+                  fullWidth
+                  size="small"
+                  name="subject"
+                    value={subject}
+                  onChange={(e) => handleInputChange(e)}
+                  inputProps={{ maxLength: 60 }}
+                />
+                <TextField
+                  variant="standard"
+                  type="text"
+                  label="Message"
+                  fullWidth
+                  multiline
+                  required
+                  maxRows={5}
+                  minRows={5}
+                  name="message"
+                    value={message}
+                  onChange={(e) => handleInputChange(e)}
+                  inputProps={{ maxLength: 400 }}
+                />
+                <div className="container-button-form">
+                  < input className="button-form-contact" type='submit' value='Send'/>
                 </div>
-              </div>
-              <TextField
-                variant="standard"
-                type="text"
-                label="Subject"
-                fullWidth
-                size="small"
-              />
-              <TextField
-                variant="standard"
-                type="text"
-                label="Message"
-                fullWidth
-                multiline
-                required
-                maxRows={5}
-                minRows={5}
-              />
-              <div className="container-button-form">
-                <button className="button-form-contact">Send</button>
-              </div>
-            </Box>
+              </Box>
+            </form>
           </div>
         </div>
       </div>
